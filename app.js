2ef648c0c32730c61718648a4d7b2afd5bf33caa
Pablo Emanuel Gillespie <103851582+pgcodedeveloper@users.noreply.github.com>
2024-03-04 13:27:19 -0300

    undo the last changes

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ conectarDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-  next();
-});
+
 //Enable the request from the client
 app.use(cors({
   origin: process.env.FRONT_END,
